98dffd05422764058425b1bb5c6ae1c153b0b8cf
4cce8er <acceber5843@hotmail.com>
2025-07-29 16:18:15 +0200

    Support groups in conv2d

--- sw/pytorch_patch/conv2d_single.py
+++ sw/pytorch_patch/conv2d_single.py
@@ -22,30 +22,36 @@ def conv2d_single(module, input):
     name = type(module).__name__
     print(Fore.GREEN + name + "[conv2d_single]" + Style.RESET_ALL)
 
-    filt = module.weight.data
+    groups = module.groups
+    filt_slices = module.weight.data.split(int(module.weight.data.size(0) / groups), dim=0)
+    input_slices = input.split(int(input.size(1) / groups), dim=1)
+
     bias = None
     if module.bias is not None:
         bias = module.bias.data
     batch_size = input.size(dim=0)
-    inputunfold = F.unfold(input,
+    inputunfold = F.unfold(input_slices[0],
                            kernel_size=module.kernel_size,
                            padding=module.padding,
                            stride=module.stride)
-    kernels_flat = filt.view(module.out_channels, -1)
+    kernels_flat = filt_slices[0].view(int(module.out_channels / groups), -1)
 
     input = module(input)
     res2 = input.clone()
 
     res2 = res2.view(batch_size,
-                     kernels_flat.size(0),
+                     kernels_flat.size(0) * groups,
                      inputunfold.size(2))
+    res2 = res2.zero_()
 
-    for m_batch in range(len(inputunfold)): # batch
-        for i in range(kernels_flat.size(0)): # out channel
-            for j in range(inputunfold.size(2)): # 'image' size
-                res2[m_batch][i][j] = 0
+    #for m_batch in range(len(inputunfold)): # batch
+    #    for i in range(kernels_flat.size(0)): # out channel
+    #        for j in range(inputunfold.size(2)): # 'image' size
+    #            res2[m_batch][i][j] = 0
+    res2_slices = res2.split(int(res2.size(1) / groups), dim=1)
 
     pbar = tqdm(total =
+                groups * 
                 len(inputunfold) *
                 kernels_flat.size(0) *
                 inputunfold.size(2) *
@@ -54,63 +60,74 @@ def conv2d_single(module, input):
     # if config.dumpon == True:
     #     _collector.initLayer(name)
 
-    for m_batch in range(len(inputunfold)): # batch
-        for i in range(kernels_flat.size(0)): # out channel
-            for j in range(inputunfold.size(2)): # 'image' size
-                pbar.update(inputunfold.size(1))
-                _chunksize = config.chunksize
-                _lsize = inputunfold.size(1)
-                if _chunksize > _lsize:
-                    _chunksize = _lsize
+    for group in range(groups):                                                   
+        filt = filt_slices[group]                                                 
+                                                                                  
+        inputunfold = F.unfold(input_slices[group],                               
+                               kernel_size=module.kernel_size,                    
+                               padding=module.padding,                            
+                               dilation=module.dilation,                          
+                               stride=module.stride)                              
+        kernels_flat = filt.view(int(module.out_channels / groups), -1) 
 
-                for k in range(0, _lsize, _chunksize): # kernel size
+        for m_batch in range(len(inputunfold)): # batch
+            for i in range(kernels_flat.size(0)): # out channel
+                for j in range(inputunfold.size(2)): # 'image' size
+                    pbar.update(inputunfold.size(1))
+                    _chunksize = config.chunksize
+                    _lsize = inputunfold.size(1)
+                    if _chunksize > _lsize:
+                        _chunksize = _lsize
 
-                    _end = k + _chunksize
-                    if _end >= _lsize:
-                        _end = _lsize
+                    for k in range(0, _lsize, _chunksize): # kernel size
 
-                    chunk_a = kernels_flat[i,k:_end]
-                    chunk_b = inputunfold[m_batch,k:_end, j]
-                    _psum = float(res2[m_batch][i][j])
+                        _end = k + _chunksize
+                        if _end >= _lsize:
+                            _end = _lsize
 
-                    if config.monkey == "ORI":
-                        c = chunk_a @ chunk_b + _psum
-                    elif config.monkey == "1_X_Y":
-                        c = kacy_fp16(chunk_a, chunk_b, _psum)
-                    else:
-                        print("wrong kacy:", config.monkey)
-                        exit(-1)
+                        chunk_a = kernels_flat[i,k:_end]
+                        chunk_b = inputunfold[m_batch,k:_end, j]
+                        _psum = float(res2_slices[group][m_batch][i][j])
 
-                    res2[m_batch][i][j] = float(c)
+                        if config.monkey == "ORI":
+                            c = chunk_a @ chunk_b + _psum
+                        elif config.monkey == "1_X_Y":
+                            c = kacy_fp16(chunk_a, chunk_b, _psum)
+                        else:
+                            print("wrong kacy:", config.monkey)
+                            exit(-1)
 
-                #     if config.dumpon == True:
-                #         _collector.sample(a, b, c, _psum)
+                        res2_slices[group][m_batch][i][j] = float(c)
 
-                # if config.dumpon == True:
-                #     _collector.doneNeuron()
+                    #     if config.dumpon == True:
+                    #         _collector.sample(a, b, c, _psum)
+
+                    # if config.dumpon == True:
+                    #     _collector.doneNeuron()
 
 
-    # for m_batch in range(len(inputunfold)): # batch
-    #     for i in range(kernels_flat.size(0)): # out channel
-    #         for j in range(inputunfold.size(2)): # 'image' size
-    #             pbar.update(inputunfold.size(1))
-    #             for k in range(inputunfold.size(1)): # kernel size
-    #                 a = float(kernels_flat[i][k])
-    #                 b = float(inputunfold[m_batch][k][j])
-    #                 _psum = float(res2[m_batch][i][j])
-    #                 if _kacy == "ORI":
-    #                     c = a * b + _psum
-    #                 elif _kacy == "1_X_Y":
-    #                     c = kacy_fp16(a, b, _psum)
-    #                 else:
-    #                     print("wrong kacy:", _kacy)
-    #                     exit(-1)
-    #                 res2[m_batch][i][j] = float(c)
-    #                 if _dumpon == True:
-    #                     _collector.sample(a, b, c, _psum)
-    #             if _dumpon == True:
-    #                 _collector.doneNeuron()
+        # for m_batch in range(len(inputunfold)): # batch
+        #     for i in range(kernels_flat.size(0)): # out channel
+        #         for j in range(inputunfold.size(2)): # 'image' size
+        #             pbar.update(inputunfold.size(1))
+        #             for k in range(inputunfold.size(1)): # kernel size
+        #                 a = float(kernels_flat[i][k])
+        #                 b = float(inputunfold[m_batch][k][j])
+        #                 _psum = float(res2[m_batch][i][j])
+        #                 if _kacy == "ORI":
+        #                     c = a * b + _psum
+        #                 elif _kacy == "1_X_Y":
+        #                     c = kacy_fp16(a, b, _psum)
+        #                 else:
+        #                     print("wrong kacy:", _kacy)
+        #                     exit(-1)
+        #                 res2[m_batch][i][j] = float(c)
+        #                 if _dumpon == True:
+        #                     _collector.sample(a, b, c, _psum)
+        #             if _dumpon == True:
+        #                 _collector.doneNeuron()
 
+    res2 = torch.cat(res2_slices, dim=1)
     res2 = res2.view(input.size())
     if bias is not None:
         bias = bias.unsqueeze(1).unsqueeze(2)
